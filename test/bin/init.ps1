7760591724a892ae76a52a1990c559cad3223662
Jakub Čábera <cabera.jakub@gmail.com>
2020-05-30 16:13:25 +0200

    refactor(tests)

--- test/bin/init.ps1
+++ test/bin/init.ps1
@@ -1,6 +1,6 @@
 Write-Host "PowerShell: $($PSVersionTable.PSVersion)"
 (7z.exe | Select-String -Pattern '7-Zip').ToString()
-Write-Host "Install dependencies ..."
+Write-Host 'Installing dependencies ...'
 Install-Module -Repository PSGallery -Scope CurrentUser -Force -Name Pester -SkipPublisherCheck
 Install-Module -Repository PSGallery -Scope CurrentUser -Force -Name PSScriptAnalyzer, BuildHelpers
 
@@ -11,9 +11,7 @@ if ($env:CI_WINDOWS -eq $true) {
         [System.Environment]::SetEnvironmentVariable('SCOOP_HELPERS', $env:SCOOP_HELPERS, 'Machine')
     }
 
-    if (!(Test-Path $env:SCOOP_HELPERS)) {
-        New-Item -ItemType Directory -Path $env:SCOOP_HELPERS
-    }
+    if (!(Test-Path $env:SCOOP_HELPERS)) { New-Item -ItemType Directory -Path $env:SCOOP_HELPERS }
     if (!(Test-Path "$env:SCOOP_HELPERS\lessmsi\lessmsi.exe")) {
         Start-FileDownload 'https://github.com/activescott/lessmsi/releases/download/v1.6.3/lessmsi-v1.6.3.zip' -FileName "$env:SCOOP_HELPERS\lessmsi.zip"
         & 7z.exe x "$env:SCOOP_HELPERS\lessmsi.zip" -o"$env:SCOOP_HELPERS\lessmsi" -y
@@ -24,17 +22,17 @@ if ($env:CI_WINDOWS -eq $true) {
     }
 }
 
-if ($env:CI -eq $true) {
-    Write-Host "Load 'BuildHelpers' environment variables ..."
+if ($env:CI -and ($env:CI -eq $true)) {
+    Write-Host 'Load ''BuildHelpers'' environment variables ...'
     Set-BuildEnvironment -Force
 }
 
-$buildVariables = ( Get-ChildItem -Path 'Env:' ).Where( { $_.Name -match "^(?:BH|CI(?:_|$)|APPVEYOR)" } )
-$buildVariables += ( Get-Variable -Name 'CI_*' -Scope 'Script' )
+$buildVariables = Get-ChildItem -Path 'Env:' | Where-Object -Property Name -Match '(?:BH|CI(?:_|$)|APPVEYOR)'
+$buildVariables = $buildVariables, (Get-Variable -Name 'CI_*' -Scope 'Script')
 $details = $buildVariables |
-    Where-Object -FilterScript { $_.Name -notmatch 'EMAIL' } |
+    Where-Object -Property Name -NotMatch 'EMAIL' |
     Sort-Object -Property 'Name' |
     Format-Table -AutoSize -Property 'Name', 'Value' |
     Out-String
-Write-Host "CI variables:"
+Write-Host 'CI variables:'
 Write-Host $details -ForegroundColor DarkGray
--- test/bin/test.ps1
+++ test/bin/test.ps1
@@ -2,30 +2,31 @@
 #Requires -Modules @{ ModuleName = 'BuildHelpers'; ModuleVersion = '2.0.1' }
 #Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
 #Requires -Modules @{ ModuleName = 'PSScriptAnalyzer'; ModuleVersion = '1.17.1' }
+
 param([String] $TestPath = 'test/')
 
-$resultsXml = "$PSScriptRoot/TestResults.xml"
-$excludes = @()
-
 $splat = @{
-    Path         = $TestPath
-    OutputFile   = $resultsXml
-    OutputFormat = 'NUnitXML'
-    PassThru     = $true
+    Path     = $TestPath
+    PassThru = $true
 }
 
-if ($env:CI -eq $true) {
+if ($env:CI -and ($env:CI -eq $true)) {
+    $excludes = @()
     $commit = if ($env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT) { $env:APPVEYOR_PULL_REQUEST_HEAD_COMMIT } else { $env:APPVEYOR_REPO_COMMIT }
     $commitMessage = "$env:APPVEYOR_REPO_COMMIT_MESSAGE $env:APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED".TrimEnd()
+    $commitChangedFiles = Get-GitChangedFile -Commit $commit
 
     if ($commitMessage -match '!linter') {
-        Write-Warning "Skipping code linting per commit flag '!linter'"
+        Write-Warning 'Skipping code linting per commit flag ''!linter'''
         $excludes += 'Linter'
     }
 
-    $changed_scripts = (Get-GitChangedFile -Include '*.ps1' -Commit $commit)
-    if (!$changed_scripts) {
-        Write-Warning "Skipping tests and code linting for *.ps1 files because they didn't change"
+    Write-Host 'changed files:'
+    $commitChangedFiles
+    exit
+
+    if ($commitChangedFiles | Where-object -Property Basename -Like '*.ps1') {
+        Write-Warning 'Skipping tests and code linting for *.ps1 files because they did not change'
         $excludes += 'Linter'
         $excludes += 'Scoop'
     }
@@ -42,19 +43,17 @@ if ($env:CI -eq $true) {
     }
 
     if ($commitMessage -match '!manifests') {
-        Write-Warning "Skipping manifest validation per commit flag '!manifests'"
+        Write-Warning 'Skipping manifest validation per commit flag ''!manifests'''
         $excludes += 'Manifests'
     }
 
-    $changed_manifests = (Get-GitChangedFile -Include '*.json' -Commit $commit)
+    $changed_manifests = Get-GitChangedFile -Include '*.json' -Commit $commit
     if (!$changed_manifests) {
-        Write-Warning "Skipping tests and validation for manifest files because they didn't change"
+        Write-Warning 'Skipping tests and validation for manifest files because they did not change'
         $excludes += 'Manifests'
     }
 
-    if ($excludes.Length -gt 0) {
-        $splat.ExcludeTag = $excludes
-    }
+    if ($excludes.Length -gt 0) { $splat.ExcludeTag = $excludes }
 }
 
 Write-Host 'Invoke-Pester' @splat
