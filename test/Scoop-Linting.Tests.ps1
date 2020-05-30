cf2959df3a896c10599040582278e40fa7f8632f
Jakub Čábera <cabera.jakub@gmail.com>
2020-05-30 19:06:52 +0200

    full

--- test/Scoop-Linting.Tests.ps1
+++ test/Scoop-Linting.Tests.ps1
@@ -4,44 +4,41 @@ Describe -Tag 'Linter' "PSScriptAnalyzer" {
         Write-Host $repo_dir -f green
 
         $scoop_modules = Get-ChildItem $repo_dir -Recurse -Include *.psd1, *.psm1, *.ps1
-        Write-Host 'ahoj', $scoop_modules.Count -f magenta
         $scoop_modules = $scoop_modules | Where-Object { $_.DirectoryName -notlike '*\supporting*' -and $_.DirectoryName -notlike '*\test*' }
-        Write-Host 'ahoj2', $scoop_modules.Count -f magenta
         $scoop_modules = $scoop_modules | Select-Object -Unique
-        Write-Host 'ahoj3', $scoop_modules.Count -f magenta
 
         $linting_settings = Get-Item -Path "$repo_dir\PSScriptAnalyzerSettings.psd1"
     }
 
     Context "Checking ScriptAnalyzer" {
         It "Invoke-ScriptAnalyzer Cmdlet should exist" {
-            { Get-Command Invoke-ScriptAnalyzer -ErrorAction Stop } | Should -not -throw
+            { Get-Command Invoke-ScriptAnalyzer -ErrorAction Stop } | Should -Not -Throw
         }
         It "PSScriptAnalyzerSettings.ps1 should exist" {
-            # Test-Path $linting_settings | Should -BeTrue
+            Test-Path $linting_settings | Should -BeTrue
         }
         It "There should be files to test" {
-            $scoop_modules.Count | Should -not -be 0
+            $scoop_modules.Count | Should -Not -Be 0
         }
     }
 
     Context "Linting all *.psd1, *.psm1 and *.ps1 files" {
-        # foreach ($directory in $scoop_modules) {
-        #     $analysis = Invoke-ScriptAnalyzer -Path $directory.FullName -Settings $linting_settings.FullName
-        #     It "Should pass: $directory" {
-        #         $analysis.Count | Should -be 0
-        #     }
-        #     if ($analysis) {
-        #         foreach ($result in $analysis) {
-        #             switch -wildCard ($result.ScriptName) {
-        #                 '*.psm1' { $type = 'Module' }
-        #                 '*.ps1' { $type = 'Script' }
-        #                 '*.psd1' { $type = 'Manifest' }
-        #             }
-        #             Write-Host -f Yellow "      [*] $($result.Severity): $($result.Message)"
-        #             Write-Host -f Yellow "          $($result.RuleName) in $type`: $directory\$($result.ScriptName):$($result.Line)"
-        #         }
-        #     }
-        # }
+        foreach ($directory in $scoop_modules) {
+            $analysis = Invoke-ScriptAnalyzer -Path $directory.FullName -Settings $linting_settings.FullName
+            It "Should pass: $directory" {
+                $analysis.Count | Should -be 0
+            }
+            if ($analysis) {
+                foreach ($result in $analysis) {
+                    switch -wildCard ($result.ScriptName) {
+                        '*.psm1' { $type = 'Module' }
+                        '*.ps1' { $type = 'Script' }
+                        '*.psd1' { $type = 'Manifest' }
+                    }
+                    Write-Host -f Yellow "      [*] $($result.Severity): $($result.Message)"
+                    Write-Host -f Yellow "          $($result.RuleName) in $type`: $directory\$($result.ScriptName):$($result.Line)"
+                }
+            }
+        }
     }
 }
